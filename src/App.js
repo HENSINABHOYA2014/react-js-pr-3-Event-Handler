30a5deaf75a92829079ca791104e51b864033c21
hensinabhoya2004 <hensinabhoya2004@gmail.com>
2023-08-08 12:39:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Data from './Data'; // Replace with your data source
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-function App() {
+function Menu() {
+  const [category, setCategory] = useState('all');
+
+  const categoryClick = (category) => {
+    setCategory(category);
+  };
+
+  const filterData = category === 'all' ? Data : Data.filter((item) => item.category === category);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 ">
+      <h1 className='text-center'>Restaurant Menu</h1>
+      <br></br>
+      <div className="text-center mb-4">
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('breakfast')}>Breakfast</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('lunch')}>Lunch</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('dinner')}>Dinner</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('started')}>Started</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('salad')}>Salad</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('dessert')}>Dessert</button>
+        <button className="btn btn-outline-primary mx-2" onClick={() => categoryClick('all')}>All</button>
+      </div>
+      <div className="row">
+        {filterData.map((item) => (
+          <div className="col-md-4 mb-4" key={item.name}>
+            <div className="card">
+              <img src={item.img} className="card-img-top object-fit-cover shadow-sm p-3 mb-5 bg-white rounded" height={250} alt={item.name} />
+              <div className="card-body ">
+                <h5 className="card-title text-success">{item.name}</h5>
+                <p className="card-text text-primary">{item.description}</p>
+                <p className="card-text text-danger">${item.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Menu;
---
+++ src/Data.jsx
@@ -0,0 +1,166 @@
+const Data=[
+    {
+        name:"Pasta",
+        category:"started",
+        price:250,
+        quantity:1,
+        description:"wow I LOVE Pasta",
+        img:"./img/15.webp"
+    },
+    {
+        name:"Nuddals",
+        category:"started",
+        price:100,
+        quantity:1,
+        description:"Wow Yummy Nuddals",
+        img:"./img/16.jpeg"
+    },
+    {
+        name:"Cheese Ball",
+        category:"started",
+        price:300,
+        quantity:1,
+        description:"Wow I Love It",
+        img:"./img/17.jpg"
+    },
+    {
+        name:"Green Salad",
+        category:"salad",
+        price:300,
+        quantity:1,
+        description:"Healathy Green salad",
+        img:"./img/18.jpg"
+    },
+    {
+        name:"Hum & Cheese Salad",
+        category:"salad",
+        price:450,
+        quantity:1,
+        description:"Cheese Salad",
+        img:"./img/19.webp"
+    },
+    {
+        name:"Greek Mezze Salad",
+        category:"salad",
+        price:200,
+        quantity:1,
+        description:"Wow I Love It",
+        img:"./img/20.avif"
+    },
+    {
+        name:"Pizza",
+        category:"dinner",
+        price:150,
+        quantity:1,
+        description:"Wow cheesy Pizza",
+        img:"./img/1.jpeg"
+    },
+    {
+        name:"Khaman",
+        category:"breakfast",
+        price:100,
+        quantity:"1Kg",
+        description:"All I Need Is Dhokala",
+        img:"./img/8.jpg"
+    },
+    {
+        name:"Idali Sambar",
+        category:"breakfast",
+        price:120,
+        quantity:"1",
+        description:"Instant Sambar",
+        img:"./img/6.jpg"
+    },  {
+        name:"juice",
+        category:"breakfast",
+        price:100,
+        quantity:"1Kg",
+        description:"Tha Taste Of nature",
+        img:"./img/13.webp"
+    },
+    {
+        name:"Udhiyu",
+        category:"lunch",
+        price:200,
+        quantity:"500gram",
+        description:"Jo Baka Udhiyu Khavanu And Maja karavani!",
+        img:"./img/4.jpg"
+    },
+    {
+        name:"RasPuri",
+        category:"lunch",
+        price:200,
+        quantity:1,
+        description:"RasPuri is good",
+        img:"./img/2.webp"
+    },
+    {
+        name:"Dhosa",
+        category:"dinner",
+        price:99,
+        quantity:5,
+        description:"Yummy Dhosa",
+        img:"./img/9.jpg"
+    },
+    {
+        name:"Pav Bhaji",
+        category:"dinner",
+        price:120,
+        quantity:5,
+        description:"Kakani Pav Bhaji",
+        img:"./img/11.jpeg"
+    },
+    {
+        name:"Poha",
+        category:"breakfast",
+        price:50,
+        quantity:1,
+        description:"A Healthy And Delicious Breakfast",
+        img:"./img/14.jpeg"
+    },
+    {
+        name:"Full Gujarati Thali",
+        category:"lunch",
+        price:400,
+        quantity:"1",
+        description:"The Taste of Bharuch",
+        img:"./img/5.jpg"
+    },
+    {
+        name: "Chesscake",
+        category: "dessert",
+        price: 250,
+        quantity: 1,
+        description: "Chesscake is so good",
+        img: "./img/7.jpg"
+    },
+    {
+        name: "Ice Cream",
+        category: "dessert",
+        price: 100,
+        quantity: "1",
+        description: "Very Delicious Ice Cream",
+        img: "./img/10.jpeg"
+    },
+    {
+        name: "Gulab Jamun",
+        category: "dessert",
+        price: 100,
+        quantity: "1",
+        description: "Very Sweert Gulab Jamun",
+        img:"./img/12.jpeg"
+    },
+    {
+        name:"Aloo Paratha",
+        category:"breakfast",
+        price:180,
+        quantity:1,
+        description:"Aloo Paratha Is Very Nice Taste",
+        img:"./img/3.jpg"
+    },
+
+    
+    
+
+]
+export default Data;
